f67c8384d68cdbf0bb975c8822f4bdbc2def7a57
afer <aferraco@sdlcpartners.com>
2021-05-05 15:11:13 -0600

    website files

---
+++ script/index.js
@@ -0,0 +1,431 @@
+var config = {
+	menu: [
+		{id: 'home', link: 'home', label: ''},
+		{id: 'network-partners', link: 'network-partners', label: 'Network Partners'},
+		{id: 'retargeting', link: 'retargeting', label: 'Retargeting'},
+		{id: 'mobile', link: 'mobile', label: 'Mobile'},
+		{id: 'ip-targeting', link: 'ip-targeting', label: 'IP Targeting'},
+		{id: 'facebook', link: 'facebook', label: 'Facebook'},
+		{id: 'smart-campus', link: 'your-business', label: 'Your Business'},
+		{id: 'contact-us', link: 'contact-us', label: 'Contact Us'}
+	],
+
+	home: [
+		{
+			'id': 'seo',
+			'header': 'Search Engine Optimization',
+			'desc': "SEO is the process of improving the volume or quality of traffic to a website from search engines via search results. SEO aims to improve rankings for relevant keywords in search results. More visitors mean more customers.",
+			'image': 'searchEngineOptimization.png'
+		},
+		{
+			'id': 'lbl',
+			'header': 'Local Business Listings',
+			'desc': "Reach Customers in your neighborhood: Our Network has more than 50 local search engines, apps, and mapping sites including Yahoo!, Yelp and Bing. Wherever your customers are searching, they'll find you.$$$Save time with one-click updates: Get listed across our Network by entering your information once. Save time and money by using PartnersinDigital Listings to correct and update your listings with one click.",
+			'image': 'localBusinessListings.png'
+		},
+		{
+			'id': 'sem',
+			'header': 'Search Engine Marketing',
+			'desc': "SEM is the process of gaining traffic by purchasing ads on search engines. It is also called paid search and sometimes referred to as PPC (pay-per-click) marketing. SEM advertising is the fastest way to drive instant, targeted traffic to your website. Target keywords on your SEM campaign are typed by users on search engines, your ads appear on top, below, or alongside organic search results.",
+			'image': 'searchEngineMarketing.png'
+		},
+		{
+			'id': 'va',
+			'header': 'Video Advertising',
+			'desc': "It's straight forward. All you need is a video to start advertising on YouTube-this can be any video you want your target audience to watch and it's cost-effective, you only pay when someone views your video.",
+			'image': 'videoAdvertising.png'
+		},
+		{
+			'id': 'fb',
+			'header': 'Facebook',
+			'desc': "Over a billion people use Facebook to connect. Based on their location, age and more, let PartnersinDigital introduce you to the people who'll love your business.$$$And, we can help you reach people on the device where they're engaged. Facebook is on three of every four smart phones and more than half of Facebook users visit the site every day.",
+			'image': 'facebook.png'
+		},
+		{
+			'id': 'rt',
+			'header': 'Retargeting',
+			'desc': "Retargeting, is an online tool that can bring \"window shoppers\" back to your site when they are ready to buy. It keeps your brand in front of visitors to your site even after they leave giving you multiple changes to bring them back. Once visitors to your site have been retargeted, they are more likely to return to make a purchase because they've shown prior interest.",
+			'image': 'retargeting.png'
+		},
+		{
+			'id': 'ipt',
+			'header': 'IP Targeting',
+			'desc': "Reach your exact online target and take your advertising to a whole new level with IP targeting. If you have an address list, we can deliver your digital message to those specific addresses.",
+			'image': 'ipTargeting.png'
+		},
+		{
+			'id': 'mw',
+			'header': 'Mobile Website',
+			'desc': "Having a website is crucial for business. So is being accessible anywhere. 57% of smartphone users search for local businesses on their device everyday. Have a professional mobile website that makes it easy for customers to find you on the go.",
+			'image': 'mobileWebsite.png'
+		},
+		{
+			'id': 'wd',
+			'header': 'Web Design',
+			'desc': "WordPress powers more than 17% of the web - a figure that rises every day. Everything from simple websites, to blogs, to complex portals and enterprise websites, and even applications, are built with WordPress. WordPress combines simplicity for users and publishers with under-the-hood complexity for developers. This makes it flexible while still being easy-to-use.",
+			'image': 'webDesign.png'
+		}
+	],
+
+	ipTarget: [
+		{
+			'id': 'wd',
+			'header': 'What is IP Targeting?',
+			'desc': "IP Targeting uses a computer's IP address to deliver targeted ads to users on web sites."
+		},
+		{
+			'id': 'wd',
+			'header': 'How does IP Targeting Work?',
+			'desc': "An advertiser provides us with a list of addresses i.e. past or current subscribers, a purchased list or any list that they maintain they want to target as potential customers. Next, we take the list and match the physical address of a person on the list to their IP or computer address. Now when one of those prospective customers goes online, we deliver a targeted ad right to the site they are viewing."
+		},
+		{
+			'id': 'wd',
+			'header': 'Does IP Targeting Use Cookies?',
+			'desc': "IP Targeting does not employ the use of cookies. So, if a user deletes the cookies on their computer, it does not affect the campaign."
+		},
+		{
+			'id': 'wd',
+			'header': 'Can I only target users at home?',
+			'desc': "No, an IP address can be for a home or business so users can be targeted when and where they are on the internet."
+		},
+		{
+			'id': 'wd',
+			'header': 'How successful is the match rate between a physical and IP address?',
+			'desc': "The matching success rate is between 50-70% meaning that a physical address is matched to an IP address between 50 and 70% of the time."
+		},
+		{
+			'id': 'wd',
+			'header': 'What are the benefits of IP Targeting?',
+			'desc': "IP Targeting allows you to target users at a household level.$$$The click-through rates for IP Targeting tend to deliver 2-3 times greater than an average display campaign.$$$List you use can be segmented to target according to demographics, interests or other attributes you have collected."
+		}
+	],
+
+	retargeting: [
+		/*{
+			'id': 'wd',
+			'header': 'What is Retargeting?',
+			'desc': "Retargeting is a cookie-based technology that uses simple Javascript code to anonymously \"follow\" your audience all over the Web. A cookie is dropped on one page or as many pages as the advertiser wants on their site."
+		},*/
+		{
+			'id': 'wd',
+			'header': 'Why Should an Advertiser Use Retargeting?',
+			'desc': "There are many benefits to Retargeting:<ul><li>Retargeting keeps an advertiser's brand in front of visitors to their site even after they leave giving the advertiser multiple chances to bring the visitor back</li><li>Retargeting targets users who already have an interest-it's a great way to capture prospects</li><li>Ads can be specific to what a user was viewing on the advertisers Web site- i.e. if they were looking at windows, a window ad can be served to them</li><li>Advertisers can choose how long to retarget</li><li>a prospect</li></ul>"
+		},
+		{
+			'id': 'wd',
+			'header': 'How is Retargeting Different from Display Advertising?',
+			'desc': "Retargeting only targets users who have had an interaction with your site and brand i.e. they have visited the advertisers site."
+		},
+		{
+			'id': 'wd',
+			'header': 'What is a cookie?',
+			'desc': "A cookie is a small text file that stores information on your computer. For retargeting, a cookie stores anonymous data about what web sites a user has visited, allowing advertisers to serve ads only to users who have been to their site before."
+		},
+		{
+			'id': 'wd',
+			'header': 'What is a Retargeting Pixel?',
+			'desc': "Used for site retargeting, a retargeting pixel is a piece of code which will be provided by PartnersinDigital for your advertiser to implement on their site. The retargeting pixel can be placed on one (1) or multiple pages but it's most often placed on the home page or \"check-out\" pages.<br/>The retargeting pixel code gives instruction to the visitor's browser to place a cookie on the visitor's device so they can be tracked.<br/>There are different kinds of retargeting pixels including a conversion and burn pixel. PartnersinDigitalwill work with your advertiser to implement the appropriate pixel."
+		},
+		{
+			'id': 'wd',
+			'header': 'How does an Advertiser Place a Retargeting Pixel on their Site?',
+			'desc': "Your advertiser will need to work with whoever maintains their web site or their internal web master to place the pixels on their pages."
+		},
+		{
+			'id': 'wd',
+			'header': 'Are There Different Types of Retargeting?',
+			'desc': "Yes, there are several different types of retargeting which you can sell to your advertisers including:<ul><li>Site Retargeting-Visitors to your Web site are tagged and then served ads reminding them about your products and services when they visit other sites</li><li>Search Retargeting-Online users are targeted that have searched for a particular keyword that is important to your company or campaign</li><li>Facebook Retargeting-Targets users who have been on the advertiser's site and have then visited Facebook. Can also target users who have liked the advertiser's Facebook page</li><li>Mobile Retargeting-Targets users on their mobile device based on recent activity</li><li>Contextual Retargeting-Targets users who are viewing certain content on a web page and then serves them ads that are highly relevant to that content</li><li>Behavioral Retargeting-Targets users on their previous actions that did not result in a sale or conversion</li><li>Demographic Retargeting-Retargets users by age or gender</li></ul>"
+		},
+		{
+			'id': 'wd',
+			'header': 'What sites are used for Retargeting?',
+			'desc': "Your advertiser's retargeting ads will appear on websites that are part of the Google DoubleClick Ad Network."
+		},
+		{
+			'id': 'wd',
+			'header': 'How is Retargeting Measured?',
+			'desc': "Retargeting is measured in two ways:<ul><li>Click-Thru Conversions-Someone Clicks on a Retargeting Ad they are served</li><li>View-Thru Conversions-Conversions that many come from another source, however, the user was served a retargeting ad</li></ul>"
+		},
+		{
+			'id': 'wd',
+			'header': 'When is Retargeting Most Effective?',
+			'desc': "Retargeting takes time to build an audience and is most effective when:<ul><li>Sites have a minimum of 1500 Unique Visitors per month</li><li>A frequency cap is set for the number of times a qualified prospect can see your ads</li><li>Creative is rotated and engaging</li><li>A unique landing page is used for the campaign</li></ul>"
+		},
+	],
+
+	mobile: [
+		/*{
+			'id': 'wd',
+			'header': 'What is the difference between a regular site and a mobile optimized site?',
+			'desc': "A regular website is usually designed for viewing on a desktop computer. When viewed on a mobile device, the user experience isn’t great. Buttons and text are smaller, navigating the site involves pinching and zooming the screen, and some elements might not work at all. A mobile-optimized site solves this problem by having thumb-friendly buttons and access to contact information and directions in just one click."
+		},*/
+		{
+			'id': 'wd',
+			'header': 'What are the benefits of having a mobile optimized site?',
+			'desc': "The main benefit of a mobile optimized site is providing a great experience to your mobile users. Over 70 million Americans browse websites from their phone every month, so providing them with quick and easy access to information about your business is essential. A mobile optimized site also helps convert mobile traffic into customers with special features like Click-to-Call and Mobile Maps with Directions."
+		},
+		{
+			'id': 'wd',
+			'header': 'What mobile devices can you use?',
+			'desc': "Works on all modern smartphones including iPhone, Android, most Blackberry devices, and Windows Mobile."
+		},
+		{
+			'id': 'wd',
+			'header': 'How do people find my mobile website?',
+			'desc': "When a user types in the URL of your regular website on their phone, they will be automatically redirected to your mobile site."
+		},
+	],
+
+	pageCache: {}
+}
+
+var click = !document.ontouchstart ? 'click' : 'touchstart mouseDown';
+
+var index = {
+
+	ctrl: {
+		init: function(){
+			index.ctrl.buildMenu();
+
+			index.ctrl.bindMenuEvents();
+
+			index.ctrl.processUrl();
+		},
+
+		buildMenu: function(){
+			var appendString = "";
+			for (var i = 1; i < config.menu.length; i++){
+				appendString += index.ui.menuTemplateHTML(i);
+
+				index.ctrl.loadPageIntoCache(config.menu[i].link);
+			}
+
+			//load home page manually - logo acts as home page link
+			index.ctrl.loadPageIntoCache('home');
+
+			$("#navBarLinks").html(appendString);
+		},
+
+		buildHomeSection: function(){
+			var appendString = "";
+			for (var i = 0; i < config.home.length; i++){
+				appendString += index.ui.homeSectionTemplateHTML(i);
+			}
+
+			$("#homeSection").html(appendString);
+		},
+
+		buildIpTargetingSection: function(){
+			var appendString = "";
+			for (var i = 0; i < config.ipTarget.length; i++){
+				appendString += index.ui.genericSectionTemplateHTML(config.ipTarget, i);
+			}
+
+			$("#ipTargetingSection").html(appendString);
+		},
+
+		buildRetargetingSection: function(){
+			var appendString = "";
+			for (var i = 0; i < config.retargeting.length; i++){
+				appendString += index.ui.genericSectionTemplateHTML(config.retargeting, i);
+			}
+
+			$("#retargetingSectionAlpha").html(appendString);
+		},
+
+		buildPartnerSection: function(){
+			var appendString = "";
+			for (var i = 0; i < config.home.length; i++){
+				appendString += index.ui.partnerSectionTemplateHTML(i);
+			}
+
+			$("#networkPartnersAlpha").html(appendString);
+		},
+
+		buildMobileSection: function(){
+			var appendString = "";
+			for (var i = 0; i < config.mobile.length; i++){
+				appendString += index.ui.genericSectionTemplateHTML(config.mobile, i);
+			}
+
+			$("#mobileSectionAlpha").html(appendString);
+		},
+
+		loadPageIntoCache: function(link){
+			$.ajax({
+				url: link + '.html',
+				async: false,
+				success: function(data){
+					config.pageCache[link] = data;
+				},
+				error: function(){
+					alert("The page failed to load");
+				}
+			});
+		},
+
+		bindMenuEvents: function(){
+			$("#navBarLinks").on(click, function(event){
+				var link = $(event.target).data("link");
+
+				if (link == null){
+					return;
+				}
+
+				event.preventDefault();
+				index.ui.loadPage(link);
+				index.ui.hideMenu();
+			});
+
+			$("#missionLink").on(click, function(event){
+				var link = 'contact-us';
+				event.preventDefault();
+				index.ui.loadPage(link);
+			});
+
+			$("#menuControl").on(click, function(event){
+				index.ui.toggleMenu();
+			});
+		},
+
+		processUrl: function(){
+			var url = document.URL;
+			var page = url.split("#/")[1];
+
+			if (config.pageCache[page]){
+				index.ui.loadPage(page);
+			}
+			else{
+				index.ui.loadPage('home');
+			}
+		}
+	},
+
+	ui: {
+		menuTemplateHTML: function(dataIndex){
+			var data = config.menu[dataIndex];
+
+			var appendString =  "<div id='" + data.id + "' class='menuItem'>";
+				appendString += "	<a href='' data-link='" + data.link + "'>" + data.label + "</a>";
+				appendString += "</div>";
+
+			return appendString;
+		},
+
+		homeSectionTemplateHTML: function(dataIndex){
+			var data = config.home[dataIndex];
+			var floatClass = dataIndex % 2 == 0 ? "float-left" : "float-right";
+
+			var appendString =  "<div class='homeSection white-container'>";
+				appendString += "	<div class='homeSectionWrapper center-content'>";
+				appendString += "		<div class='condensedHomeSection'>";
+				appendString += "			<div class='homeSectionTextContainer " + floatClass + "'>";
+				appendString += "				<h2 class='homeSectionHeader light-blue-text'>" + data.header + "</h2>";
+				appendString += "				<div class='homeSectionDesc'>" + index.ui.parseDesc(data.desc) + "</div>";
+				appendString += "			</div>";
+				appendString += "			<div class='homeSectionImageContainer " + floatClass + "'>";
+				appendString += "				<img class='homeSectionImage' src='images/"+data.image+"' />";
+				appendString += "			</div>";
+				appendString += "		</div>";
+
+				appendString += "		<div class='largeHomeSection'>";
+				appendString += "			<table width='100%'><thead></thead><tbody><tr>";
+
+				if (dataIndex % 2 == 0){
+					appendString += "	<td width='40%'><img class='homeSectionImage' src='images/"+data.image+"' /></td>";
+					appendString += "	<td width='60%'><h2 class='homeSectionHeader light-blue-text'>" + data.header + "</h2><div class='homeSectionDesc'>" + index.ui.parseDesc(data.desc) + "</div></td>";
+				}
+				else{
+					appendString += "	<td width='60%'><h2 class='homeSectionHeader light-blue-text'>" + data.header + "</h2><div class='homeSectionDesc'>" + index.ui.parseDesc(data.desc) + "</div></td>";
+					appendString += "	<td width='40%'><img class='homeSectionImage' src='images/"+data.image+"' /></td>";
+				}
+
+				appendString += "		</tr></tbody></table></div>";
+
+				appendString += "	</div>";
+				appendString += "</div>";
+
+			return appendString;
+		},
+
+		genericSectionTemplateHTML: function(list, dataIndex){
+			var data = list[dataIndex];
+
+			var appendString =  "<div class='homeSection'>";
+				appendString += "	<div class='homeSectionWrapper'>";
+				appendString += "		<div class='homeSectionTextContainer ipTargetingTextContainer'>";
+				appendString += "			<h2 class='homeSectionHeader light-blue-background'>" + data.header + "</h2>";
+				appendString += "			<div class='indentSectionDesc'>" + index.ui.parseDesc(data.desc) + "</div>";
+				appendString += "		</div>";
+				appendString += "	</div>";
+				appendString += "</div>";
+
+			return appendString;
+		},
+
+		partnerSectionTemplateHTML: function(dataIndex){
+			var data = config.home[dataIndex];
+
+			var appendString =  "<div class='homeSection white-container'>";
+				appendString += "	<table width='100%'><thead></thead><tbody>";
+				appendString += "		<tr>";
+				appendString += "			<td width='50%'><img class='homeSectionImage' src='images/"+data.image+"' /></td>";
+				appendString += "			<td width='50%'><div class='homeSectionDesc'>" + index.ui.parseDesc(data.desc) + "</div></td>";
+				appendString += "		</tr>";
+				appendString += "	</tbody></table>";
+				appendString += "</div>";
+
+			return appendString;
+		},
+
+		parseDesc: function(text){
+			var split = text.split("$$$");
+			var returnText = "";
+
+			for (var i = 0; i < split.length; i++){
+				if (split[i].trim() != ""){
+					returnText += "<p>" + split[i] + "</p>";
+				}
+			}
+
+			return returnText;
+		},
+
+		loadPage: function(link){
+			var html = config.pageCache[link];
+
+			$("#pageContent").html(html);
+
+			window.location = "#/" + link;
+		},
+
+		toggleMenu: function(){
+			if ($(window).width() > 600){
+				return;
+			}
+
+			var left = parseInt($(".headerContainer").css("left"), 10);
+
+			if (left < 0){
+				//$("#menuControl").html("Hide Links");
+				index.ui.showMenu();
+			}
+			else{
+				index.ui.hideMenu();
+				//$("#menuControl").html("Show Links");
+			}
+		},
+
+		showMenu: function(){
+			$(".headerContainer").css("left", "0px");
+		},
+
+		hideMenu: function(){
+			$(".headerContainer").css("left", "");
+		}
+	}
+}
+
+$(function(){
+	index.ctrl.init();
+});
